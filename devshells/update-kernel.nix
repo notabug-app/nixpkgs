f1a07251f4e6f1fa7a7eb915f4ea84d4f5f0cfdf
s-Sizz <71391035+s-Sizz@users.noreply.github.com>
2026-08-04 17:20:01 +0300

    feat: add update-kernel script to devshell

---
+++ devshells/update-kernel.nix
@@ -0,0 +1,76 @@
+{ pkgs }:
+
+pkgs.writeShellApplication {
+  name = "update-kernel";
+  runtimeInputs = with pkgs; [
+    git
+    curl
+    jq
+    gnused
+    nix
+  ];
+  text = ''
+    set -euo pipefail
+
+    if [ "$#" -ne 1 ]; then
+        echo "Usage: update-kernel <base-version>"
+        echo "Example: update-kernel 7.1.y"
+        exit 1
+    fi
+
+    BASE_VERSION=$1
+    BRANCH="rpi-''${BASE_VERSION}"
+    REPO="https://github.com/raspberrypi/linux"
+
+    echo "Fetching latest commit for branch $BRANCH..."
+    COMMIT=$(git ls-remote "$REPO.git" "refs/heads/$BRANCH" | awk '{print $1}')
+    if [ -z "$COMMIT" ]; then
+        echo "Error: Branch $BRANCH not found"
+        exit 1
+    fi
+    echo "Latest commit: $COMMIT"
+
+    echo "Fetching Makefile to determine modDirVersion..."
+    MAKEFILE_URL="https://raw.githubusercontent.com/raspberrypi/linux/''${COMMIT}/Makefile"
+    MAKEFILE_CONTENT=$(curl -sSL "$MAKEFILE_URL")
+
+    VERSION=$(echo "$MAKEFILE_CONTENT" | grep -E '^VERSION = ' | awk '{print $3}')
+    PATCHLEVEL=$(echo "$MAKEFILE_CONTENT" | grep -E '^PATCHLEVEL = ' | awk '{print $3}')
+    SUBLEVEL=$(echo "$MAKEFILE_CONTENT" | grep -E '^SUBLEVEL = ' | awk '{print $3}')
+
+    if [ -z "$VERSION" ] || [ -z "$PATCHLEVEL" ] || [ -z "$SUBLEVEL" ]; then
+        echo "Error parsing Makefile for version"
+        exit 1
+    fi
+
+    MOD_DIR_VERSION="''${VERSION}.''${PATCHLEVEL}.''${SUBLEVEL}"
+    echo "Kernel version: $MOD_DIR_VERSION"
+
+    echo "Calculating nix hash... (this may take a while)"
+    PREFETCH_JSON=$(nix store prefetch-file --json --name source --unpack "''${REPO}/archive/''${COMMIT}.tar.gz")
+    SRI_HASH=$(echo "$PREFETCH_JSON" | jq -r '.hash')
+
+    if [ -z "$SRI_HASH" ] || [ "$SRI_HASH" = "null" ]; then
+        echo "Error getting hash."
+        exit 1
+    fi
+
+    echo "Hash: $SRI_HASH"
+
+    echo "Updating linux.nix and flake.nix..."
+
+    MAJOR=$(echo "$BASE_VERSION" | cut -d. -f1)
+    MINOR=$(echo "$BASE_VERSION" | cut -d. -f2)
+    NEW_SUFFIX="''${MAJOR}_''${MINOR}"
+
+    sed -i -E "s/modDirVersion = \".+\";/modDirVersion = \"$MOD_DIR_VERSION\";/" linux.nix
+    sed -i -E "s/tag = \".+\";/tag = \"$COMMIT\";/" linux.nix
+    sed -i -E "s|srcHash = \".+\";|srcHash = \"$SRI_HASH\";|" linux.nix
+    # shellcheck disable=SC2016
+    sed -i -E 's/name = "linuxPackages_rpi\$\{model\}_[0-9]+_[0-9]+";/name = "linuxPackages_rpi\''${model}_'"''${NEW_SUFFIX}"'";/' linux.nix
+
+    sed -i -E "s/linuxPackages_rpi([45])_[0-9]+_[0-9]+/linuxPackages_rpi\1_''${NEW_SUFFIX}/g" flake.nix
+
+    echo "Done!"
+  '';
+}
