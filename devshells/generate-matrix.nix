15d1662d235b6c16a29f2e21f096ae95eebee40e
s-Sizz <71391035+s-Sizz@users.noreply.github.com>
2026-08-17 21:44:56 +0300

    ci: refactor build workflow to use custom action and prioritize kernels

--- devshells/generate-matrix.nix
+++ devshells/generate-matrix.nix
@@ -10,7 +10,7 @@ pkgs.writeShellApplication {
       exit 1
     fi
     # shellcheck disable=SC2016
-    PKGS=$(nix eval --json .#packages --apply 'packages: builtins.concatLists (map (system: let os = if system == "aarch64-linux" then "ubuntu-24.04-arm" else "ubuntu-24.04"; in map (package: { inherit system package os; }) (builtins.filter (p: p != "firn") (builtins.attrNames packages.''${system}))) [ "aarch64-linux" "x86_64-linux" ])')
+    PKGS=$(nix eval --json .#packages --apply 'packages: builtins.concatLists (map (system: let os = if system == "aarch64-linux" then "ubuntu-24.04-arm" else "ubuntu-24.04"; allPkgs = builtins.filter (p: p != "firn") (builtins.attrNames packages.''${system}); isKernel = p: builtins.match ".*kernel.*" p != null; kernels = builtins.filter isKernel allPkgs; others = builtins.filter (p: ! (isKernel p)) allPkgs; in map (package: { inherit system package os; }) (kernels ++ others)) [ "aarch64-linux" "x86_64-linux" ])')
     echo "matrix={\"include\":$PKGS}" >> "$GITHUB_OUTPUT"
   '';
 }
