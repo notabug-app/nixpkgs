a6617dc841f21db8d8efc8fbe44989d64667fdb9
s-Sizz <71391035+s-Sizz@users.noreply.github.com>
2026-08-19 23:27:19 +0300

    fix(linux.nix): force disable desktop DRM drivers
    Nixpkgs common-config.nix forces many DRM modules to be built. We must use mkForce to actually strip them out of the Pi kernel.

--- linux.nix
+++ linux.nix
@@ -16,11 +16,11 @@ let
         modDirVersion = "7.2.0";
         tag = "48599d5d6403fd9680b2f5582a7b7b17a0c9d018";
         srcHash = "sha256-rBR1UMSicc7QyRcaBK9i5jqt1/EgorpWaBWeTJYfcY8=";
-        structuredExtraConfig = with prev.lib.kernel; {
-          DRM_I915 = no;
-          DRM_AMDGPU = no;
-          DRM_RADEON = no;
-          DRM_NOUVEAU = no;
+        structuredExtraConfig = with prev.lib; with prev.lib.kernel; {
+          DRM_I915 = mkForce no;
+          DRM_AMDGPU = mkForce no;
+          DRM_RADEON = mkForce no;
+          DRM_NOUVEAU = mkForce no;
         };
         kernelPatches = [
           {
